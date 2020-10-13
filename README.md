289ac681fe6f7abad6011c65b2e425d8b0eb9213
23aartale <23aartale@athenian.org>
2020-10-13 15:35:31 +0000

    updated

--- README.md
+++ README.md
@@ -8,26 +8,3 @@
     * 2 WR's(wide reciever's)
     * 1 TE (Tight End)
     * 1 K (Kicker)
-## How Is The Season Like:
-### There will be a season of 10 games.
-* **How many teams are in the league?**
-    * **There is 1 conference and the 9 teams are:**
-        * Olympia Oxens
-        * Milwakuee Monarchs
-        * Nashville Narwhals
-        * Columbia Camels
-        * St. Paul Snakes
-        * San Jose Salmon
-        * Carson City Cats
-        * Cheyenne Shrimp
-        * Boulder Baboons
-## How do the games work?
-#### The games work in a fashion in which your team will be faced against an oppenent and before the matchups the screen
-#### Will show the odds of you winning and if you choose to accept those odds then the game will run and tell you the score
-#### of the match. Then the game will record into your record and you play the next game. If you choose to decline the odds
-#### then that game will count as a tie. You have up to 3 games to decline.
-
-## What Happens after the 9 games?
-#### The code will run a simulation of the other games that the other teams will play on your same matchup day.
-#### If you have the best record then congratulations! If you don't then there is always next season!
-#### Now, I wish you the best luck with your draft and season!
--- my_code.py
+++ my_code.py
@@ -1,6 +1,5 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  none
-import random
+#  Collaborators: Parker, Milan
 def subtract(bank):
     bank = 100000
 
@@ -223,13 +222,9 @@ def choose_again(redo):
     print(TE_Pick)
     print(K_Pick)
 
-if int(bank) < 0:
+if int(subtract(bank)) < 0:
     print(choose_again(redo))
 
-print("WELCOME TO FANTASAY FOOTBALL!")
-team_name = input("Enter your fantasay team name: ")
-
-#QB CHOICE
 print("I like it! Select your QB for this league: ")
 print("Lamar Jackson $17,000")
 print("Patrick Mahomes $20,000")
@@ -259,7 +254,7 @@ while QB_Pick == 'Lamar Jackson' or QB_Pick == 'Patrick Mahomes' or QB_Pick =='R
 print("Good Choice!")
 print("You have " + str(bank) + " left in your bank")
 
-        #RB CHOICE
+    #RB CHOICE
 print("Now time for your RB choice:")
 print("Christian Mcaffery $20,000")
 print("Ezekiel Elliot $17,000")
@@ -290,7 +285,7 @@ print("Good Choice!")
 print("You currently have " + str(bank) + " left in your bank")
 
 
-#RB2 CHOICE
+    #RB2 CHOICE
 print("Now time for your second running back:")
 print("Melvin Gordon $10,000")
 print("Raheem Mostert $7000")
@@ -299,26 +294,26 @@ print("Nick Chubb $11000")
 print("Kareem Hunt $9000")
 print("Clyde Edward-Helaire $12000")
 print("James Robinson $12000")
-        RB2_Pick = input()
-        if RB2_Pick == 'Melvin Gordon':
-            bank = int(bank) - 10000
-        elif RB2_Pick == 'Raheem Mostert':
-            bank = int(bank) - 7000
-        elif RB2_Pick == 'Todd Gurley':
-            bank = int(bank) - 9000
-        elif RB2_Pick == 'Nick Chubb':
-            bank = int(bank) - 11000
-        elif RB2_Pick == 'Kareem Hunt':
-            bank = int(bank) - 9000
-        elif RB2_Pick == 'Clyde Edward-Helaire':
-            bank = int(bank) - 12000
-        elif RB2_Pick == "James Robinson":
-            bank = int(bank) - 12000
+RB2_Pick = input()
+if RB2_Pick == 'Melvin Gordon':
+    bank = int(bank) - 10000
+elif RB2_Pick == 'Raheem Mostert':
+    bank = int(bank) - 7000
+elif RB2_Pick == 'Todd Gurley':
+    bank = int(bank) - 9000
+elif RB2_Pick == 'Nick Chubb':
+    bank = int(bank) - 11000
+elif RB2_Pick == 'Kareem Hunt':
+    bank = int(bank) - 9000
+elif RB2_Pick == 'Clyde Edward-Helaire':
+    bank = int(bank) - 12000
+elif RB2_Pick == "James Robinson":
+    bank = int(bank) - 12000
 print("Good Choice!")
 print("You currently have " + str(bank) + " left in your bank")
 
 
-        #WR1 Choice
+    #WR1 Choice
 print("Now your choice for your first wide reciever")
 print("DeAndre Hopkins $17000")
 print("Micheal Thomas $20000")
@@ -326,24 +321,24 @@ print("Devante Adams $19000")
 print("Adam Thielen $16500")
 print("Julio Jones $17000")
 print("Tyreek Hill $15000")
-        WR1_Pick = input()
-        if WR1_Pick == 'DeAndre Hopkins':
-            bank = int(bank) - 17000
-        elif WR1_Pick == 'Micheal Thomas':
-            bank = int(bank) - 20000
-        elif WR1_Pick == 'Devante Adams':
-            bank = int(bank) - 19000
-        elif WR1_Pick == 'Adam Thielen':
-            bank = int(bank) - 16500
-        elif WR1_Pick == 'Julio Jones':
-            bank = int(bank) - 17000
-        elif WR1_Pick == 'Tyreek Hill':
-            bank = int(bank) - 15000
+WR1_Pick = input()
+if WR1_Pick == 'DeAndre Hopkins':
+    bank = int(bank) - 17000
+elif WR1_Pick == 'Micheal Thomas':
+    bank = int(bank) - 20000
+elif WR1_Pick == 'Devante Adams':
+    bank = int(bank) - 19000
+elif WR1_Pick == 'Adam Thielen':
+    bank = int(bank) - 16500
+elif WR1_Pick == 'Julio Jones':
+    bank = int(bank) - 17000
+elif WR1_Pick == 'Tyreek Hill':
+    bank = int(bank) - 15000
 print("Good Choice!")
 print("You currently have " + str(bank) + " left in your bank")
 
 
-        #WR2 CHOICE
+    #WR2 CHOICE
 print("Now choose your second Wide Reciever:")
 print("Jamison Crowder $10000")
 print("Cooper Kupp $11000")
@@ -355,32 +350,32 @@ print("Amari Cooper $13000")
 print("Tyler Lockett $13000")
 print("Mike Evans $12000")
 print("DK Metcalf $10000")
-        WR2_Pick = input()
-        if WR2_Pick == 'Jamison Crowder':
-            bank = int(bank) - 10000
-        elif WR2_Pick == 'Cooper Kupp':
-            bank = int(bank) - 11000
-        elif WR2_Pick == 'Juju':
-            bank = int(bank) - 10000
-        elif WR2_Pick == 'Tyler Boyd':
-            bank = int(bank) - 9000
-        elif WR2_Pick == 'AJ Brown':
-            bank = int(bank) - 9000
-        elif WR2_Pick == 'Stefon Diggs':
-            bank = int(bank) - 11000
-        elif WR2_Pick == 'Amari Cooper':
-            bank = int(bank) - 13000
-        elif WR2_Pick == 'Tyler Lockett':
-            bank = int(bank) - 13000
-        elif WR2_Pick == 'Mike Evans':
-            bank = int(bank) - 12000
-        elif WR2_Pick == 'DK Metcalf':
-            bank = int(bank) - 10000
+WR2_Pick = input()
+if R2_Pick == 'Jamison Crowder':
+    bank = int(bank) - 10000
+elif WR2_Pick == 'Cooper Kupp':
+    bank = int(bank) - 11000
+elif WR2_Pick == 'Juju':
+    bank = int(bank) - 10000
+elif WR2_Pick == 'Tyler Boyd':
+    bank = int(bank) - 9000
+elif WR2_Pick == 'AJ Brown':
+    bank = int(bank) - 9000
+elif WR2_Pick == 'Stefon Diggs':
+    bank = int(bank) - 11000
+elif WR2_Pick == 'Amari Cooper':
+    bank = int(bank) - 13000
+elif WR2_Pick == 'Tyler Lockett':
+    bank = int(bank) - 13000
+elif WR2_Pick == 'Mike Evans':
+    bank = int(bank) - 12000
+elif WR2_Pick == 'DK Metcalf':
+    bank = int(bank) - 10000
 print("Good Choice")
-print("You currently have " + str(bank) + " left in your bank")
+print("You currently have " + str(bank) + " left in yourbank")
 
 
-        #TE CHOICE
+    #TE CHOICE
 print("Now time for your tight end choice:")
 print("Travis Kelce $15000")
 print("George Kittle $14000")
@@ -389,26 +384,26 @@ print("Mark Andrews $12000")
 print("Zach Ertz $10000")
 print("Jimmy Graham $9000")
 print("Hunter Henry $10000")
-        TE_Pick = input()
-        if TE_Pick == 'Travis Kelce':
-            bank = int(bank) - 15000
-        elif TE_Pick == 'George Kittle':
-            bank = int(bank) - 14000
-        elif TE_Pick == 'Darren Waller':
-            bank = int(bank) - 11000
-        elif TE_Pick == 'Mark Andrews':
-            bank = int(bank) - 12000
-        elif TE_Pick == 'Zach Ertz':
-            bank = int(bank) - 10000
-        elif TE_Pick == 'Jimmy Graham':
-            bank = int(bank) - 9000
-        elif TE_Pick == 'Hunter Henry':
-            bank = int(bank) - 10000
+TE_Pick = input()
+if TE_Pick == 'Travis Kelce':
+    bank = int(bank) - 15000
+elif TE_Pick == 'George Kittle':
+    bank = int(bank) - 14000
+elif TE_Pick == 'Darren Waller':
+    bank = int(bank) - 11000
+elif TE_Pick == 'Mark Andrews':
+    bank = int(bank) - 12000
+elif TE_Pick == 'Zach Ertz':
+    bank = int(bank) - 10000
+elif TE_Pick == 'Jimmy Graham':
+    bank = int(bank) - 9000
+elif TE_Pick == 'Hunter Henry':
+    bank = int(bank) - 10000
 print("Good Choice!")
 print("You currently have " + str(bank) + " left in your bank")
 
 
-        #Kicker Choice
+    #Kicker Choice
 print("Now time for you to pick your kicker:")
 print("Harrison Butker $10000")
 print("Will Lutz $8000")
@@ -417,21 +412,21 @@ print("Justin Tucker $9000")
 print("Greg Zurelein $7000")
 print("Roberto Blankenship $9000")
 print("Chris Boswell $7000")
-        K_Pick = input()
-        if K_Pick == 'Harrison Butker':
-            bank = int(bank) - 10000
-        elif K_Pick == 'Will Lutz':
-            bank = int(bank) - 8000
-        elif K_Pick == 'Stephen Gostkowski':
-            bank = int(bank) - 7000
-        elif K_Pick == 'Justin Tucker':
-            bank = int(bank) - 9000
-        elif K_Pick == 'Greg Zurelein':
-            bank = int(bank) - 7000
-        elif K_Pick == 'Roberto Blankenship':
-            bank = int(bank) - 9000
-        elif K_Pick == 'Chris Boswell':
-            bank = int(bank) - 7000
+K_Pick = input()
+if K_Pick == 'Harrison Butker':
+    bank = int(bank) - 10000
+elif K_Pick == 'Will Lutz':
+    bank = int(bank) - 8000
+elif K_Pick == 'Stephen Gostkowski':
+    bank = int(bank) - 7000
+elif K_Pick == 'Justin Tucker':
+    bank = int(bank) - 9000
+elif K_Pick == 'Greg Zurelein':
+    bank = int(bank) - 7000
+elif K_Pick == 'Roberto Blankenship':
+    bank = int(bank) - 9000
+elif K_Pick == 'Chris Boswell':
+    bank = int(bank) - 7000
 print("Good Choice!")
 print("You currently have " + str(bank) + " left in your bank")
 
